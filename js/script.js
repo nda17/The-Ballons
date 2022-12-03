97cdcd3e66da2063c535fa4469c8a9d4a59b7d3d
Dmitry Nefedov <104081901+nfdmitry@users.noreply.github.com>
2022-12-04 06:58:11 +1000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 document.querySelector('.nav').onclick = function() {
-  document.querySelector('.menu-mobile').classList.toggle('active');
+  document.querySelector('.nav').classList.toggle('active');
   document.querySelector('.nav-main').classList.toggle('active');
   document.querySelector('.nav-main-list').classList.toggle('active');
 };
